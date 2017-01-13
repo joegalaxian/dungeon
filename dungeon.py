ef425028f3629d7b8bed75e8ccbc7eec02d3127c
joegalaxian <joegalaxian@users.noreply.github.com>
2017-01-13 14:29:52 +1100

    Add files via upload

--- dungeon.py
+++ dungeon.py
@@ -32,9 +32,16 @@ class Game(object):
 		self.level = Level()
 		return
 
+
 	def run(self):
 		while not self.is_completed:
-			self.level.run()
+			while not self.level.is_completed:
+				self.level.run()
+			self.next_level()
+
+
+	def next_level(self):
+		self.level = Level(self.level.floor_no + 1)
 
 
 # Class Level
@@ -50,11 +57,15 @@ class Level(object):
 
 	# Loads map as list of strings from file.
 	def load_map_file(self):
-		map_file_path = 'maps/' + str(self.floor_no) + '.map'
-		map_file = open(map_file_path, 'r')
-		m = map_file.readlines()
-		map_file.close()
-		return m
+		try:
+			map_file_path = 'maps/' + str(self.floor_no) + '.map'
+			map_file = open(map_file_path, 'r')
+			m = map_file.readlines()
+			map_file.close()
+			return m
+		except IOError:
+			terminate("Game over, you've hit the end of the dungeon!")
+
 
 	# Loads map as dictionary from list of strings. Dictionary structure: m{(x,y): 'c'}
 	def load_map_as_dict(self, lst_lines):
@@ -63,7 +74,7 @@ class Level(object):
 		x = len(lst_lines)
 		y = len(lst_lines[0])-1
 		
-		print '>DEGUG x,y (lst_lines):', (x,y)
+		# print '>DEGUG x,y (lst_lines):', (x,y)
 		for xi in range(x):
 			for yi in range(y):
 				"""
@@ -82,22 +93,11 @@ class Level(object):
 
 
 	def run(self):
-		self.render()
-		self.tick()
-		#self.render()
+		while not self.is_completed:
+			self.render()
+			self.tick()
+			#self.render()
 		
-
-	def tick(self):
-		# Read and action key:
-		key = read_key()
-		action_key(key, self.map_matrix)
-
-		"""
-		# TODO:
-		from obj in self.objects:
-			obj.run()
-		"""
-
 
 	def render(self):
 		# Clears screen, prints status, and prints the map.
@@ -115,7 +115,7 @@ class Level(object):
 		print 'Dungeon.py v01 \r'
 		#print "time......:", time
 		print "floor.....:", self.floor_no, '\r'
-		print "player....:", get_key(self.map_matrix, 'P'), '\r'
+		print "player....:", self.get_map_position('P'), '\r'
 		#print "key.......:", key
 		print "map_size..:", self.map_size(), '\r'
 
@@ -130,112 +130,112 @@ class Level(object):
 		return (x+1,y+1)
 
 
+	def tick(self):
+		# Read and action key:
+		key = self.read_key()
+		self.action_key(key)
+
+		"""
+		# TODO:
+		from obj in self.objects:
+			obj.run()
+		"""
+
+
+	# Returns int value of key pressed.
+	def read_key(self):
+		# Config the keyboard as raw to read key press.
+		attr = termios.tcgetattr(sys.stdin)
+		tty.setraw(sys.stdin)
+		k = sys.stdin.read(1)
+		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, attr)
+		return ord(k[0])
 
 
 
+	# Action key (i.e. move player, help menu, quit game, etc).
+	def action_key(self, k):
 
+		# New (desired) position
+		nx, ny = self.new_position(k)
 
+		# Common object positions.
+		px, py = self.get_map_position('P')	# Player
+		switch = self.get_map_position('s')	# Switch
+		exit = self.get_map_position('_')	# Exit
+		
+		
+		# If ESC, Q, q pressed then terminate:
+		if k in KEY_ESC:
+			terminate()
 
-
-
-
-
-
-
-
-
-
-# Returns int value of key pressed.
-def read_key():
-	# Config the keyboard as raw to read key press.
-	attr = termios.tcgetattr(sys.stdin)
-	tty.setraw(sys.stdin)
-	c = sys.stdin.read(1)
-	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, attr)
-	return ord(c[0])
-
-
-def new_position((x,y), k):
-	if k in KEY_UP:
-		return (x-1,y)
-	elif k in KEY_DOWN:
-		return (x+1,y)
-	elif k in KEY_LEFT:
-		return (x,y-1)
-	elif k in KEY_RIGHT:
-		return (x,y+1)
-
-# Action key (i.e. move player, help menu, quit game, etc).
-def action_key(k, m):
-	switch = get_key(m, 's')
-	px, py = get_key(m, 'P')
-
-	# If ESC, Q, q pressed then terminate:
-	if k in KEY_ESC:
-		terminate()
-
-	# If hit the wall then ring.
-	if k in (KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT):
-		nx, ny = new_position((px,py), k)
-
-		if m[nx,ny] == 'X':
+		# If moves towards the exit then enter new level.
+		#if (nx, ny) == exit:
+		if self.map_matrix[(nx, ny)] == '_':
+			self.next_level()
+	
+		# If moves towards the wall then ring.
+		if self.map_matrix[nx,ny] == 'X':
 			print '\a'
-		elif m[nx,ny] == '_':
-			print "Passed!"
+
+		# If moves towards hit an empty space
+		if self.map_matrix[(nx, ny)] == '.':
+			self.map_matrix[nx, ny] = 'P'
+			self.map_matrix[px, py] = '.'
+
+		# KEY_SPACE:
+		if k in KEY_SPACE:
+			return	# Wait a turn.
+	
+		# KEY_YES:
+	
+		# KEY_NO:
+	
+		# KEY_TAB:
+	
+		# KEY_HELP:
+		if k in KEY_HELP:
+			print "Help:\r"
+			print "[W,A,S,D]...Move player.\r"
+			print "[ESC,Q].....Terminate game.\r"
+			print "[SPACE].....Wait a turn.\r"
+			#print "Press any key to resume game."
+			#read_key()
+	
+		if k in KEY_LEVEL:
+			#self.is_completed = True
+			self.next_level()
+	
 		else:
 			None
 
-	# If KEY_UP:
-	if k in KEY_UP:
-		if m[px-1, py] == '.':
-			m[px-1, py] = 'P'
-			m[px, py] = '.'
 
-	# KEY_LEFT:
-	if k in KEY_LEFT:
-		# If empty space then move there.
-		if m[px, py-1] == '.':
-			m[px, py-1] = 'P'
-			m[px, py] = '.'
+	# Returns position (x,y) of the valuer on the map matrix.
+	def get_map_position(self, value):
+		for (k, v) in self.map_matrix.iteritems():
+			if v == value:
+				return k
 
-	# KEY_DOWN:
-	if k in KEY_DOWN:
-		# If empty space then move there.
-		if m[px+1,py] == '.':
-			m[px+1,py] = 'P'
-			m[px,py] = '.'
+	# Return the new (desired) player position after the key pressed.
+	def new_position(self, k):
+		x, y = self.get_map_position('P')
+		if k in KEY_UP:
+			return x-1, y
+		elif k in KEY_DOWN:
+			return x+1, y
+		elif k in KEY_LEFT:
+			return x, y-1
+		elif k in KEY_RIGHT:
+			return x, y+1
+		else:
+			return x, y
 
-	# KEY_RIGHT:
-	if k in KEY_RIGHT:
-		# If empty space then move there.
-		if m[px,py+1] == '.':
-			m[px,py+1] = 'P'
-			m[px,py] = '.'
 
-	# KEY_SPACE:
-	if k in KEY_SPACE:
-		return	# Wait a turn.
-
-	# KEY_YES:
-
-	# KEY_NO:
-
-	# KEY_TAB:
-
-	# KEY_HELP:
-	if k in KEY_HELP:
-		print "Help:"
-		print "Move player with W, A, S, D."
-		print "Terminate the game with ESC, Q, q."
-		print "Wait a turn with SPACE."
-		#print "Press any key to resume game."
-		#read_key()
-
-	if k in KEY_LEVEL:
+	# Mark level as completed.	
+	def next_level(self):
 		self.is_completed = True
 
-	else:
-		None
+
 
 
 # Terminates game printing a message, 'Game terminated.' by default.
@@ -243,51 +243,8 @@ def terminate(str = "Game terminated."):
 	print str
 	quit()
 
-# Returns position (x,y) of the player on the map.
-def locate_player(m):
-	for x in range(len(m)):
-		for y in range(len(m[0])):
-			if m[x][y] == 'P':
-				return (x,y)
-
-
-# Returns position (x,y) of the player on the dictionary map.
-def get_key(dic, value):
-	for (k, v) in dic.iteritems():
-		if v == value:
-			return k
-
-
-"""
-# Main function.
-def main():
-	key = None
-	time = 0
-	floor = 0
-
-	# Game main loop
-	while True:
-
-		# Load new level:
-		floor += 1
-		level = Level(floor)
-
-		# Loop on level.
-		while not level.is_completed:
-
-			g.render_map_dict(level.map_dict)
-
-			# Read and action key:
-			key = read_key()
-			#action_key(key, level.map_dict)
-			level.action_key(key)
-			time += 1
-
-			if key in KEY_LEVEL: level.completed = True
-"""
-
 
 if __name__ == '__main__':
 	game = Game()
 	game.run()
-	#main()
+
